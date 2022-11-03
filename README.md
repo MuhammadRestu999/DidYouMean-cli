add65222f47c214a8e8ec0fe8726a58a6a249d1e
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-11-03 13:17:55 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+didYouMean-cli - CLI version of didyoumean.js
+===================================================
+
+[Available on GitHub](https://github.com/MuhammadRestu999/DidYouMean-cli.git).
+
+
+Examples
+--------
+
+From file
+```
+## list.txt
+## hello world, foo bar, foo, bar, Hello world, Hello World
+dym "hello worls" list.txt # hello world
+dym "Hello worls" --cs list.txt # Hello world
+
+
+#$ list.json
+## ["hello world", "foo bar", "foo", "bar", "Hello world", "Hello World"]
+dym "hello worls" list.json # hello world
+dym "Hello worls" --cs list.json # Hello world
+```
+
+
+dym text file options
+----------------------------
+
+- text: The string input to match.
+- file: An file of strings or array to match against.
+- options (OPTIONAL)
+
+Returns: the closest matching string, or nothing if no strings exceed the threshold.
+
+
+Options
+-------
+
+Options are set on the dym arguments. You may change them at any time.
+
+### case-sensitive
+
+  By default, the method will perform case-insensitive comparisons. If you wish to force case sensitivity, add
+  --case-sensitive or --cs to options
+
+  ```
+  dym text file --case-sensitive
+  ```
+
+
+Special thanks
+--------------
+- [dcporter](https://github.com/dcporter): for [didyoumean.js](https://github.com/dcporter/didyoumean.js)
+- [yeoman](https://github.com/yeoman): [update-notifier](https://github.com/yeoman/update-notifier)
+
+
+License
+-------
+
+Copyright (c) 2022 Muhammad Restu
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
---
+++ main.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+(async function() {
+  const package = require("./package");
+  try {
+    const dym = require("didyoumean");
+    const updateNotifier = (await import("update-notifier")).default;
+    const fs = require("fs");
+
+    updateNotifier({
+      updateCheckInterval: 0,
+      pkg: package
+    }).notify();
+
+    let [, cmd, text, file, ...options] = process.argv;
+    cmd = cmd.split("/").at(-1);
+
+    const options_list = [
+      [
+        [
+          "case-sensitive",
+          "cs"
+        ],
+        "By default, the method will perform case-insensitive comparisons. If you wish to force case sensitivity, add --case-sensitive or --cs to options"
+      ]
+    ];
+    const accept = [".json", ".txt"];
+    const hint = `
+${package.name}@${package.version} By ${package.author}
+
+  Usage: ${cmd} <text> <file> (options)
+
+  List options:
+${options_list.map(([name, desc]) => "    " + (Array.isArray(name) ? name.map(v => (v.length == 1 ? "-" : "--") + v).join(", ") : (name.length == 1 ? "-" : "--") + name) + "\n      " + desc)}
+
+  Supported file:
+    txt:
+      list 1, list 2, list 3
+
+      list 1
+      list 2
+      list 3
+    json:
+      ["list 1", "list 2", "list 3"]
+
+  Example:
+    $ ${cmd} "Hello worls" example.txt --cs
+    Hello world
+`;
+
+    if(process.argv.length < 4) return console.log(hint);
+    if(options.some(v => options_list.some(([name]) => name == v.replace(/[^0-9A-z]/gi, "")))) return console.log(hint);
+    if(accept.every(v => !file.endsWith(v))) return console.log(hint);
+    if(!fs.existsSync(file)) return console.log(`${cmd}: cannot access \'${file}\': No such file or directory`);
+
+    let content = fs.readFileSync(file)+"";
+    if(file.endsWith(".json")) content = new Array(...new Set(JSON.parse(content).filter(v => typeof v == "string" || typeof v == "number").map(v => ""+v)));
+
+    if(file.endsWith(".json") && !Array.isArray(content)) return console.log(hint);
+    if(file.endsWith(".txt")) {
+      if(content.includes("\n")) content = content.split("\n");
+      else content = content.split(", ");
+    };
+
+    if(options.includes("--case-sensitive") || options.includes("--cs")) dym.caseSensitive = true;
+
+    let result = dym(text, content);
+    if(result) console.log(result);
+  } catch(e) {
+    if(e.code == "EACCES") return console.log(`${cmd}: cannot access \'${file}\': Permission denied`);
+    if(e.code == "EISDIR") return console.log(`${cmd}: ${file}: Is a directory`);
+
+    if(e?.message?.includes?.("JSON")) return console.log(`${cmd}: ${file}: Is not a JSON`);
+
+    console.log("Error!", `Report this log to ${package.bugs.url} :\n`);;
+    console.error(e);
+    return;
+  };
+})();
